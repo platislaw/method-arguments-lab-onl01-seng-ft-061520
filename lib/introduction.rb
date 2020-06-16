b42b7071fcddce8c1286803c2bef472f6df57519
Paul Latislaw <paullatislaw@gmail.com>
2020-06-16 02:39:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,11 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction("Josh")
+
 
 def introduction_with_language (name, language)
   puts "Hi my mane is #{name} and I am learning to program in #{language}."
 end
 
+introduction("Josh")
 introduction_with_language ("Dan", "Embers.js")
